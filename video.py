b18e39c8122133924215cbb8e09c57e5d2eb47a1
Karishma-Joseph <karishmajoseph70@gmail.com>
2020-11-21 17:39:10 -0700

    Convert images to video using ffmpeg

---
+++ video.py
@@ -0,0 +1,10 @@
+# import os
+# os.system("echo Hello from the other side!")
+
+# ffmpeg -r 1/5 -i img%03d.jpeg -c:v libx264 -vf "fps=25,format=yuv420p" -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" out.mp4
+
+import subprocess
+
+list_dir = subprocess.Popen(["ffmpeg", "-r", "1/5", "-i", "img%03d.jpeg", "-i", "bbt.mp3", "-c:v", "libx264", "-vf", "fps=25,format=yuv420p", "-vf", "pad=ceil(iw/2)*2:ceil(ih/2)*2", "out.mp4"])
+list_dir.wait()
+
